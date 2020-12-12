ea8d040dca1a4d00c68ddb0a3dbe15bd53838494
Nehemiah Boone <nehemiah.boone@gmail.com>
2020-12-11 17:39:23 -0700

    post model

---
+++ Models/Post.cs
@@ -0,0 +1,11 @@
+namespace CLReddit.Models
+{
+  public class Post
+  {
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Text { get; set; }
+    public int Upvotes { get; set; }
+    public int Downvotes { get; set; }
+  }
+}
